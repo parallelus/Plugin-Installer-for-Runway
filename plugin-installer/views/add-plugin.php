caf8a9e6f991f3cd3d375ec8feaa7135b1c2fca2
Alexx G <alexxgermann@yandex.ru>
2016-08-10 16:52:47 +0300

    Refactoring for views

--- plugin-installer/views/add-plugin.php
+++ plugin-installer/views/add-plugin.php
@@ -1,28 +1,30 @@
 <?php
-	if(isset($info_message) && $info_message != "") {
-		?>
-<div class="error"><?php echo  $info_message; ?></div>
-		<?php
-	}
+if ( isset( $info_message ) && $info_message != "" ) {
+	?>
+
+	<div class="error"><?php echo $info_message; ?></div>
+
+	<?php
+}
 ?>
 
-<h2><?php _e('Upload a File', 'runway'); ?></h2>
+<h2><?php _e( 'Upload a File', 'runway' ); ?></h2>
 
 <h4><?php _e( 'Copy a plugin in .zip format to Plugin Installer plugins folder', 'runway' ) ?></h4>
 <p class="install-help"><?php _e( 'If you have a plugin in a .zip format, you may copy it by uploading it here.', 'runway' ) ?></p>
 <form method="post" enctype="multipart/form-data" action="<?php echo self_admin_url( 'admin.php?page=plugin-installer&navigation=add-plugin' ) ?>">
 	<?php wp_nonce_field( 'plugin-upload-action', 'plugin-upload-field' ) ?>
 	<label class="screen-reader-text" for="plugzip"><?php _e( 'Plugin zip file', 'runway' ); ?></label>
-	<input type="file" id="plugzip" name="plugzip" />
-	<input type="submit" class="button" value="<?php esc_attr_e( 'Copy Now', 'runway' ) ?>" name="plug-submit" />
+	<input type="file" id="plugzip" name="plugzip"/>
+	<input type="submit" class="button" value="<?php esc_attr_e( 'Copy Now', 'runway' ) ?>" name="plug-submit"/>
 </form>
 
-<h2><?php _e('OR', 'runway'); ?></h2>
+<h2><?php _e( 'OR', 'runway' ); ?></h2>
 
-<h2><?php _e('Select a Plugin from the WordPress Repository', 'runway'); ?></h2>
+<h2><?php _e( 'Select a Plugin from the WordPress Repository', 'runway' ); ?></h2>
 
 <form method="post" action="<?php echo self_admin_url( 'admin.php?page=plugin-installer&navigation=add-plugin-by-url' ); ?>">
 	<p class="install-help"><?php _e( 'Plugin URL', 'runway' ) ?></p>
-	<input type="text" id="plugin_url" name="plugin_url" />
-	<input type="submit" class="button" value="<?php _e('Select Plugin', 'runway'); ?>" name="plugin_url_submit" />
+	<input type="text" id="plugin_url" name="plugin_url"/>
+	<input type="submit" class="button" value="<?php _e( 'Select Plugin', 'runway' ); ?>" name="plugin_url_submit"/>
 </form>
--- plugin-installer/views/extensions.php
+++ plugin-installer/views/extensions.php
@@ -1,61 +1,82 @@
-<form action="<?php echo esc_url($this->self_url('extensions') .'&action=install-extension'); ?>" method="post"><br>
-<table class="wp-list-table widefat plugins">
-	<thead>
+<form action="<?php echo esc_url( $this->self_url( 'extensions' ) . '&action=install-extension' ); ?>" method="post">
+	<br>
+	<table class="wp-list-table widefat plugins">
+		<thead>
 		<tr>
-			<th scope="col" id="cb" class="manage-column column-cb check-column" style="width: 0px;"><input type="checkbox" name="ext_chk[]" /></th>
-			<th id="name" class="manage-column column-name"><?php echo __('Extension', 'runway'); ?></th>
-			<th id="description" class="manage-column column-description"><?php echo __('Description', 'runway'); ?></th>
-		</tr>
-	</thead>
-	<tbody id="the-list">
-	<?php
-if ( !empty( $exts ) ):
-	foreach ( $exts as $ext => $ext_info ):
-		$ext_cnt = !$extm->is_activated( $ext );
-?>
-		<?php if($extm->is_activated( $ext )): ?>
-		<tr class="inactive">
-			<th class="check-column">
-				<input type="checkbox" name="ext_chk[]" value="<?php echo esc_attr($ext); ?>" />
+			<th scope="col" id="cb" class="manage-column column-cb check-column" style="width: 0px;">
+				<input type="checkbox" name="ext_chk[]"/>
 			</th>
-			<td class="plugin-title">
-				<strong><?php rf_e($ext_info['Name']); ?></strong>
-			</td>
-			<td class="column-description desc">
-				<?php
-					// Item description
-					$description = '<div class="plugin-description"><p>'. rf__($ext_info['Description']) .'</p></div>';
-					// Item info
-					$class = ( $ext_cnt ) ? 'inactive' : 'active' ;
-					$version = ( $ext_info['Version'] ) ? __('Version', 'runway').': '.$ext_info['Version'] : '';
-					if ( $ext_info['Author'] ) {
-						$author = ' | By '. $ext_info['Author'];
-						if ( $ext_info['AuthorURI'] ) {
-							$author = ' | By <a href="'. esc_url($ext_info['AuthorURI']) .'" title="'.__('Visit author homepage', 'runway').'">'. $ext_info['Author'] .'</a>';
-						}
-					}
-					else {
-						$author = ' | By Unknown';
-					}
-					$plugin_link = ( $ext_info['ExtensionURI'] ) ? ' | <a href="'. esc_url($ext_info['ExtensionURI']) .'" title="'. esc_attr( __('Visit plugin site', 'runway') ).'">'.__('Visit plugin site', 'runway').'</a>' : '';
-					$info = '<div class="'. esc_attr($class) .' second plugin-version-author-uri">'. $version . $author . $plugin_link .'</div>';
+			<th id="name" class="manage-column column-name"><?php echo __( 'Extension', 'runway' ); ?></th>
+			<th id="description" class="manage-column column-description"><?php echo __( 'Description', 'runway' ); ?></th>
+		</tr>
+		</thead>
+		<tbody id="the-list">
 
-					// Print details
-					echo  $description; // escaped above
-					echo  $info; // escaped above
+		<?php
+		if ( ! empty( $exts ) ):
+			foreach ( $exts as $ext => $ext_info ):
+				$ext_cnt = ! $extm->is_activated( $ext );
 				?>
-			</td>
-		</tr>
-	<?php endif; endforeach; else: ?>
-		<tr calss="active">
-			<td class="plugin-title">
-				<?php echo __('Extensions not found', 'runway'); ?>.
-			</td>
-			<td class="column-description desc"> </td>
-		</tr>
-	<?php endif; ?>
-	</tbody>
-</table><br>
+				<?php if ( $extm->is_activated( $ext ) ): ?>
 
-<input class="button-primary" type="submit" value="<?php echo __('Allow Install as Plugin', 'runway'); ?>">
+				<tr class="inactive">
+					<th class="check-column">
+						<input type="checkbox" name="ext_chk[]" value="<?php echo esc_attr( $ext ); ?>"/>
+					</th>
+					<td class="plugin-title">
+						<strong><?php rf_e( $ext_info['Name'] ); ?></strong>
+					</td>
+					<td class="column-description desc">
+
+						<?php
+						// Item description
+						$description = '<div class="plugin-description"><p>' . rf__( $ext_info['Description'] ) . '</p></div>';
+						// Item info
+						$class   = ( $ext_cnt ) ? 'inactive' : 'active';
+						$version = ( $ext_info['Version'] ) ? __( 'Version', 'runway' ) . ': ' . $ext_info['Version'] : '';
+						if ( $ext_info['Author'] ) {
+							$author = ' | By ' . $ext_info['Author'];
+							if ( $ext_info['AuthorURI'] ) {
+								$author = ' | By <a href="' . esc_url( $ext_info['AuthorURI'] ) .
+								          '" title="' . __( 'Visit author homepage', 'runway' ) . '">' .
+								          $ext_info['Author'] . '</a>';
+							}
+						} else {
+							$author = ' | By Unknown';
+						}
+						$plugin_link = ( $ext_info['ExtensionURI'] ) ?
+							' | <a href="' . esc_url( $ext_info['ExtensionURI'] ) .
+							'" title="' . esc_attr( __( 'Visit plugin site', 'runway' ) ) . '">' .
+							__( 'Visit plugin site', 'runway' ) . '</a>'
+							: '';
+						$info        = '<div class="' .
+						               esc_attr( $class ) . ' second plugin-version-author-uri">' .
+						               $version . $author . $plugin_link .
+						               '</div>';
+
+						// Print details
+						echo $description; // escaped above
+						echo $info; // escaped above
+						?>
+
+					</td>
+				</tr>
+
+			<?php endif; endforeach;
+		else: ?>
+
+			<tr calss="active">
+				<td class="plugin-title">
+					<?php echo __( 'Extensions not found', 'runway' ); ?>.
+				</td>
+				<td class="column-description desc"></td>
+			</tr>
+
+		<?php endif; ?>
+
+		</tbody>
+	</table>
+	<br>
+
+	<input class="button-primary" type="submit" value="<?php echo __( 'Allow Install as Plugin', 'runway' ); ?>">
 </form>
